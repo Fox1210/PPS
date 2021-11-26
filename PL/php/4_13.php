9932491134b0c804e159f3a672d51cf71ba46afb
fox1210 <pablogomezmelendez@gmail.com>
2021-11-26 21:31:53 +0100

    limpieza ejercicio 4.13

--- PL/php/4_13.php
+++ PL/php/4_13.php
@@ -1,12 +1,9 @@
-$v[1]=90;
-$v[30]=7;
-$v['e']=99;
-$v['hola']=43;
+<!--HECHO POR PABLO GÓMEZ MELÉNDEZ-->
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
     <head>
         <meta content="text/html; charset=ISO-8859-1" httpequiv="content-type">
-        <title>Ejercicio 4.13</title>
+        <title>Ejercicio 4.13 PGM</title>
     </head>
     <body>
         <?php
@@ -19,22 +16,17 @@ $v['hola']=43;
             // echo “El elemento de indice $indice vale $valor <br>”;
             // }
             //
-            echo "-------------------------------Meses de un año-------------------------------";
+            echo "<br>-------------------------------Meses de un año-------------------------------<br>";
             $vector=["enero","febrero","marzo","abril","mayo",
                     "junio","julio","agosto","septiembre","octubre","noviembre","diciembre"];
             foreach ($vector as $indice => $valor) {
-                echo "El mes en el indice $indice vale $valor";
+                echo "El mes en el indice $indice vale $valor <br>";
             }
 
             /*listado de dias: para cada mes cuales son sus dias, ejemplo: enero:31,
             febrero:28... $listado_dias["enero"]=31 ---> echo "El mes de mes tiene x dias" **Array bidimensional*/
-            echo "-------------------------------Meses de un año-------------------------------";
-            $vector=["enero","febrero","marzo","abril","mayo",
-                    "junio","julio","agosto","septiembre","octubre","noviembre","diciembre"];
-            $vector=[[1],]
-            foreach ($vector as $i => $valor) {
-                echo "El mes en el indice $i vale $valor";
-            }
+            echo "<br>-------------------------------Meses de un año-------------------------------<br>";
+            
 
         ?>
     </body>

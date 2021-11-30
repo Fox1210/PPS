4c6e63bc14bb466ec9e0166b0eb9ac8a559356f3
fox1210 <pablogomezmelendez@gmail.com>
2021-11-30 18:05:25 +0100

    Modificaciones de estructura

---
+++ Práctica1.3/php/5_1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+
+<head>
+    <meta content="text/html; charset=ISO-8859-1" httpequiv="content-type">
+    <title>ejercicio 5.1</title>
+</head>
+
+<body>
+    <?php
+    import_request_variables("pg", "f_");
+    echo "Son ";
+    echo $f_euros * 166.386;
+    echo " pesetas";
+    ?>
+</body>
+
+</html>

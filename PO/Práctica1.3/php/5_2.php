e74b151a1adcf9e5228e497fc71feeba59bd6ace
fox1210 <pablogomezmelendez@gmail.com>
2021-12-02 16:05:07 +0100

    5.2 Original

---
+++ PO/Práctica1.3/php/5_2.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+
+<head>
+    <meta content="text/html; charset=ISO-8859-1" httpequiv="content-type">
+    <title>ejercicio 5.2</title>
+</head>
+
+<body>
+    <?php
+    // import_request_variables("pg", "f_");//Funcion deprecated
+    extract($_POST, EXTR_PREFIX_ALL,'f');
+    echo "Son ";
+    if ($f_conv == 1) {
+        echo $f_cantidad / 166.386;
+        echo " euros";
+    } else {
+        echo $f_cantidad / 180.386;
+        echo " dolares";
+    }
+    ?>
+</body>
+
+</html>

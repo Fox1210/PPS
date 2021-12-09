d2abbf8a5734ecba5a282717ba7f85546d790513
fox1210 <pablogomezmelendez@gmail.com>
2021-12-09 16:33:24 +0100

    Ejercicio 5_3

---
+++ PO/Práctica1.3/php/5_3.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+
+<body>
+    <?php
+    if (is_uploaded_file($_FILES['foto']['tmp_name'])) {
+        $nombre = date(DATE_RFC822);
+        print($nombre);
+        copy($_FILES['foto']['tmp_name'], "fotos/$nombre.jpg");
+    } else
+        echo "Possible file upload attack. Filename: " .
+            $_FILES['foto']['name'] . "---" . $_FILES['foto']['tmp_name'];
+    ?>
+</body>
+
+</html>

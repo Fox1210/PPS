ff5b11743faf2f06edc7651902b2964c3f81498f
fox1210 <pablogomezmelendez@gmail.com>
2021-12-09 16:32:57 +0100

    modificación de 5.4

--- PO/Práctica1.3/php/5_4.php
+++ PO/Práctica1.3/php/5_4.php
@@ -5,7 +5,7 @@
     <meta content="text/html; charset=ISO-8859-1" httpequiv="content-type">
     <title>Ejercicio 5.4</title>
 </head>
-
+<!-- //TODO Hecer que sea excluyente, es decir que el formulario solo se muestre una vez -->
 <body>
     <?php
     // import_request_variables("pg", "f_"); //!Funcion obsoleta
@@ -16,10 +16,10 @@
             echo $f_cantidad / 166.386 . " euros<BR>";
         else
             echo $f_cantidad / 180.386 . " dolares<BR>";
-    }
+    }else{
     ?>
     <form method=post action=5_4.php>
-        Introduzca la cantidad: <input type=text name=cantidad size=10>
+        Introduzca la cantidad: <input type=text name=cantidad size=10> Pesetas
         <input type=submit name=ok value=enviar>
         <br>
         Seleccione el tipo de conversion:<br>
@@ -27,6 +27,10 @@
         <input type=radio name=conv value=2>dolares<br>
         <input type=hidden name=estado value=1>
     </form>
+    <?php
+    }
+    ?>
+    
 </body>
 
 </html>

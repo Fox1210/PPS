a1923347d6e5309cb833b2cd237c0e7c2785885c
fox1210 <pablogomezmelendez@gmail.com>
2021-12-09 16:50:33 +0100

    Modificación 5.3

--- PO/Práctica1.3/php/5_3.php
+++ PO/Práctica1.3/php/5_3.php
@@ -4,9 +4,11 @@
 <body>
     <?php
     if (is_uploaded_file($_FILES['foto']['tmp_name'])) {
-        $nombre = date(DATE_RFC822);
+        // $nombre = date(DATE_RFC822);//! Da problemas por que añade espacios
+        $nombre="aaa".rand();
         print($nombre);
-        copy($_FILES['foto']['tmp_name'], "fotos/$nombre.jpg");
+        // copy($_FILES['foto']['tmp_name'], "fotos/$nombre.jpg");//! Da problemas dla barra en windows, no en linux
+        copy($_FILES['foto']['tmp_name'], "..\\picture\\$nombre.jpg");
     } else
         echo "Possible file upload attack. Filename: " .
             $_FILES['foto']['name'] . "---" . $_FILES['foto']['tmp_name'];

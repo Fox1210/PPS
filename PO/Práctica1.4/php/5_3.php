db3cc364e08d110b50a8ba592c9b2e4c4c61d68f
fox1210 <pablogomezmelendez@gmail.com>
2021-12-15 19:24:03 +0100

    fail commend

--- PO/Práctica1.4/php/5_3.php
+++ PO/Práctica1.4/php/5_3.php
@@ -19,7 +19,7 @@
         copy($_FILES['foto']['tmp_name'], "..\\picture\\$nombre.jpg");
         echo "";
 
-        ///// //TODO Si el tamano es mayor al indicado en el hiden se elimina
+        ///// //TODO Si el tamaño es mayor al indicado en el hiden se elimina
         if ($sizeMax<$sizePicture) {
             unlink("..\\picture\\$nombre.jpg");
         }

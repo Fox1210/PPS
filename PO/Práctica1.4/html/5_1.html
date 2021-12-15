daf484f42da6ef17867032f2ae0b7d821c67e336
fox1210 <pablogomezmelendez@gmail.com>
2021-12-15 17:33:23 +0100

    firmar modificados

--- PO/Práctica1.4/html/5_1.html
+++ PO/Práctica1.4/html/5_1.html
@@ -1,3 +1,4 @@
+<!--HECHO POR PABLO GÓMEZ MELÉNDEZ-->
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
 
--- PO/Práctica1.4/php/5_2.php
+++ PO/Práctica1.4/php/5_2.php
@@ -1,3 +1,4 @@
+<!--HECHO POR PABLO GÓMEZ MELÉNDEZ-->
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
 
@@ -8,20 +9,28 @@
 
 <body>
     <?php
+    function saltoLinea()
+    {
+        echo "<br>";
+    }
     // import_request_variables("pg", "f_");//Funcion deprecated
     extract($_POST, EXTR_PREFIX_ALL, 'f');
     echo "Son ";
     if ($f_conv == 1) {
         echo $f_cantidad / 166.386;
-        echo " euros";
+        echo " euros <br>";
     } else {
         echo $f_cantidad / 180.386;
-        echo ' dolares';
+        echo ' dolares <br>';
     }
     echo $f_password;
+    saltaLinea();
     echo $f_checkbox;
+    saltaLinea();
     echo $f_hidden;
+    saltaLinea();
     echo $f_textarea;
+    saltaLinea();
     echo $f_select;
 
     echo "*************************";
